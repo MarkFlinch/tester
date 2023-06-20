36fb0f84dd67f9fffdc4489a0d22ff197943530e
Mark Flinch <Flinchmark1@gmail.com>
2023-06-20 15:07:55 -0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,7 @@
   import create from "$lib/images/creativevizions.png";
 </script>
 
-<Navbar name="TBBV" />
-<Hero
-  text="CREATIVE VIZIONS"
-  p="My art is lively, energetic, and covers an assortment of genres. I use perspective and movement
-when creating my work to bring attention to my characters. The overall focus of my art is to
-explore the generic use of photography and videography but through animation and digital 2d.
-Furthermore, my goal for the future is to expand my knowledge on animation and start
-producing short films, then eventually full episodes using dynamic perspective and camera
-movement."
-  img={create}
-  alt_txt="T B B Visions"
-/>
+<img src={create} alt="ninedd" />
 
 <style>
 </style>
